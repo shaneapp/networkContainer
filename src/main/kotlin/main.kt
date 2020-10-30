74b672040dd49768ff31aa2b721c79f30c88bdbf
shaneapp <shaneapp@hotmail.com>
2020-10-30 15:43:08 +0000

    Add failing test for security feature of networking library

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -2,3 +2,8 @@
 fun main(args: Array<String>) {
     println("hello world")
 }
+
+interface IHttpClient
+interface Network {
+    fun protocolAllowed(endpoint: String): Boolean
+}
--- src/test/kotlin/NetworkTests.kt
+++ src/test/kotlin/NetworkTests.kt
@@ -4,4 +4,18 @@ import org.junit.Test
 
 class NetworkTests {
 
+    private val secureEndpoint = "https://randomapi.com"
+    private val unsecureEndpoint = "http://randomapi.com"
+
+    @Test
+    fun testSecureClient() {
+        val unsecureNetwork: Network = mockk()
+        assertEquals(unsecureNetwork.protocolAllowed(secureEndpoint), true)
+        assertEquals(unsecureNetwork.protocolAllowed(unsecureEndpoint), true)
+
+        val secureNetwork: Network = mockk()
+        assertEquals(secureNetwork.protocolAllowed(secureEndpoint), true)
+        assertEquals(secureNetwork.protocolAllowed(unsecureEndpoint), false)
+    }
+
 }
