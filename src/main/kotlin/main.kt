df097161b074ef7b0ab497d9cc870edc1fc9bff0
shaneapp <shaneapp@hotmail.com>
2020-11-01 10:44:54 +0000

    add failing test for decoding responses

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,26 +1,50 @@
-import kotlin.random.Random
+import sun.security.ssl.Debug
 
 fun main(args: Array<String>) {
     println("hello world")
 }
 
 class NetworkContainer(private val httpClient: IHttpClient) : Network {
+    override fun requestSync(endpoint: String, httpMethod: HttpMethod, responseDecoder: IResponseDecoder<DebugResponse>): DebugResponse {
+        TODO("Not yet implemented")
+    }
+
     override fun protocolAllowed(endpoint: String): Boolean {
         return httpClient.validateDomain(endpoint)
     }
 }
 
 class UnsecureHttpClient : IHttpClient {
+    override fun performFetch(): ByteArray {
+        TODO("Not yet implemented")
+    }
+
     override fun validateDomain(endpoint: String): Boolean = endpoint.startsWith("http")
 }
 
 class SecureHttpClient : IHttpClient {
+    override fun performFetch(): ByteArray {
+        TODO("Not yet implemented")
+    }
+
     override fun validateDomain(endpoint: String) = endpoint.startsWith("https")
 }
 
 interface IHttpClient {
+    fun performFetch(): ByteArray
     fun validateDomain(endpoint: String): Boolean
 }
+
 interface Network {
     fun protocolAllowed(endpoint: String): Boolean
+    fun requestSync(endpoint: String, httpMethod: HttpMethod, responseDecoder: IResponseDecoder<DebugResponse>): DebugResponse
 }
+
+enum class HttpMethod { GET, POST, PUT, DELETE }
+
+interface IResponseDecoder<T: IDecoderResult> {
+    fun deserialize(rawResult: ByteArray): T
+}
+
+interface IDecoderResult
+data class DebugResponse(val debugOutput: String) : IDecoderResult
--- src/test/kotlin/NetworkTests.kt
+++ src/test/kotlin/NetworkTests.kt
@@ -1,4 +1,6 @@
-import io.mockk.mockk
+import io.mockk.every
+import io.mockk.mockkObject
+import io.mockk.verify
 import junit.framework.Assert.assertEquals
 import org.junit.Test
 
@@ -16,6 +18,28 @@ class NetworkTests {
         val secureNetwork = NetworkContainer(SecureHttpClient())
         assertEquals(secureNetwork.protocolAllowed(secureEndpoint), true)
         assertEquals(secureNetwork.protocolAllowed(unsecureEndpoint), false)
+    }
+
+    @Test
+    fun testDecoderDeserializesResponse() {
+        val network = NetworkContainer(object : IHttpClient {
+            override fun performFetch() = "Test response".toByteArray()
+            override fun validateDomain(endpoint: String) = true
+        })
+
+        val debugDecoder = object : IResponseDecoder<DebugResponse> {
+            override fun deserialize(rawResult: ByteArray): DebugResponse {
+                return DebugResponse(rawResult.toString(Charsets.UTF_8))
+            }
+        }
+
+        mockkObject(network)
+        every { network.protocolAllowed(any()) } returns true
+
+        val postResult = network.requestSync(secureEndpoint, HttpMethod.POST, debugDecoder)
+
+        verify(exactly = 1) { network.requestSync(any(), any(), any()) }
+        assertEquals(postResult.debugOutput, "Test response")
     }
 
 }
